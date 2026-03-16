318c8a8829c038094e4a6be77c3efdf9e56b31ae
Kefilwe Lourenco <kefilwebr@hotmail.com>
2026-03-16 16:22:48 -0300

    Implementaão da classe Controller e métodos CRUD

--- Menu.ts
+++ Menu.ts
@@ -1,68 +1,193 @@
 import readlinesync = require("readline-sync");
 import { LivroFisico } from "./src/model/LivroFisico";
 import { Ebook } from "./src/model/Ebook";
+import { ProdutoController } from "./src/controller/ProdutoController";
 
 export function main() {
+ 
+let produtos: ProdutoController = new ProdutoController();
 
-let opcao: number; 
-
-//Linhas testes
+/*Linhas testes
 const livrofisico: LivroFisico =  new LivroFisico(1, 1, "JavaScript O guia definitivo 7ed.", "David Flanagan", 204.40, 704);
 livrofisico.visualizar();
 
 const ebook: Ebook = new Ebook(2, 2, "Lógica de Programação e Algoritmos com JavaScript: uma introdução à Programação de Computadores com exemplos e Exercícios Para Iniciante", "Edécio Fernando Iepsen", 352, 4.60);
-ebook.visualizar();
+ebook.visualizar(); */
 
-while (true) {
+let opcao, id, tipo, paginas: number;
+let tamanhoArquivo, preco: number;
+let titulo, autor: string;
+const tiposLivros = ['Livro Físico', 'Ebook'];
 
-    console.log("***************************************");
-    console.log("      LIVRARIA DIGITAL GENERATION      ");
-    console.log("***************************************");
+console.log("\nCriar Produtos\n");
 
-    console.log("1 - Cadastrar Livro");
-    console.log("2 - Listar Livros");
-    console.log("3 - Buscar Livro por ID");
-    console.log("4 - Atualizar Livro");
-    console.log("5 - Deletar Livro");
-    console.log("6 - Sair");
-    console.log("                                       ");
-    console.log("***************************************");
+    let lf1: LivroFisico = new LivroFisico(produtos.gerarId(), 1, "JavaScript O guia definitivo 7ed.", "David Flanagan", 204.40, 704);
+    produtos.cadastrar(lf1);
 
-    console.log("\nDigite uma opção: ");
-    opcao = readlinesync.questionInt("");
+    let lf2: LivroFisico = new LivroFisico(produtos.gerarId(), 1, "Entendendo Algoritmos", "Aditya Bhargava", 87.90, 264);
+    produtos.cadastrar(lf2);
 
-    switch (opcao) {
-        case 1:
-            console.log("\nCadastrar Livro\n")
-        break;
+    let eb1: Ebook = new Ebook(produtos.gerarId(), 2, "Lógica de Programação e Algoritmos com JavaScript", "Edécio Fernando Iepsen", 352.00, 4.6);
+    produtos.cadastrar(eb1);
 
-        case 2:
-            console.log("\nListar Livros\n");
-        break;
+    let eb2: Ebook = new Ebook(produtos.gerarId(), 2, "Pequeno Manual Antirracista", "Djamila Ribeiro", 29.90, 2.1);
+    produtos.cadastrar(eb2);
 
-        case 3:
-            console.log("\nBuscar Livro por ID\n");
-        break;
+    produtos.listarTodos();
 
-        case 4:
-            console.log("\nAtualizar Livro\n");
-        break;
+    while (true) {
 
-        case 5: 
-            console.log("\nDeletar Livro\n")
-        break;
+        console.log("***************************************");
+        console.log("      LIVRARIA DIGITAL GENERATION      ");
+        console.log("***************************************");
 
-        case 6:
-            console.log("\nSaindo da Livraria Digital Generation...\n");
+        console.log("1 - Cadastrar Livro"                    );
+        console.log("2 - Listar Livros"                      );
+        console.log("3 - Buscar Livro por ID"                );
+        console.log("4 - Atualizar Livro"                    );
+        console.log("5 - Deletar Livro"                      );
+        console.log("6 - Sair"                               );
+        console.log("                                       ");
+        console.log("***************************************");
+
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 6) {
+            console.log("\nLivraria Digital Generation - leitura para todo mundo!");
+            sobre();
             process.exit(0);
+        }
 
-        default:
-            console.log("\nOpção Inválida! Tente novamente...");
-        break;
+        switch (opcao) {
+            case 1:
+                console.log("\n\nCadastrar Livro\n");
 
+                console.log("Digite o Título do Livro: ");
+                titulo = readlinesync.question("");
+
+                console.log("Digite o Autor do Livro: ");
+                autor = readlinesync.question("");
+
+                console.log("\nDigite o tipo do Livro: ");
+                tipo = readlinesync.keyInSelect(tiposLivros, "", { cancel: false }) + 1;
+
+                console.log("\nDigite o Preço do Livro (R$): ");
+                preco = readlinesync.questionFloat("");
+
+                switch (tipo) {
+                    case 1:
+                        console.log("Digite o Número de páginas: ");
+                        paginas = readlinesync.questionInt("");
+                        produtos.cadastrar(
+                            new LivroFisico(produtos.gerarId(), tipo, titulo, autor, preco, paginas)
+                        );
+                        break;
+
+                    case 2:
+                        console.log("Digite o Tamanho do arquivo (MB): ");
+                        tamanhoArquivo = readlinesync.questionFloat("");
+                        produtos.cadastrar(
+                            new Ebook(produtos.gerarId(), tipo, titulo, autor, preco, tamanhoArquivo)
+                        );
+                        break;
+                }
+
+                keyPress();
+                break;
+
+            case 2:
+                console.log("\n\nListar todos os Livros\n");
+                produtos.listarTodos();
+
+                keyPress();
+                break;
+
+            case 3:
+                console.log("\n\nBuscar Livro por ID\n");
+
+                console.log("Digite o ID do Livro: ");
+                id = readlinesync.questionInt("");
+                produtos.procurarPorId(id);
+
+                keyPress();
+                break;
+
+            case 4:
+                console.log("\n\nAtualizar Dados do Livro\n");
+
+                console.log("Digite o ID do Livro: ");
+                id = readlinesync.questionInt("");
+
+                let produto = produtos.buscarNoArray(id);
+
+                if (produto != null) {
+
+                    console.log("Digite o Título do Livro: ");
+                    titulo = readlinesync.question("");
+
+                    console.log("Digite o Autor do Livro: ");
+                    autor = readlinesync.question("");
+
+                    tipo = produto.tipo;
+
+                    console.log("\nDigite o Preço do Livro (R$): ");
+                    preco = readlinesync.questionFloat("");
+
+                    switch (tipo) {
+                        case 1:
+                            console.log("Digite o Número de páginas: ");
+                            paginas = readlinesync.questionInt("");
+                            produtos.atualizar(
+                                new LivroFisico(id, tipo, titulo, autor, preco, paginas)
+                            );
+                            break;
+
+                        case 2:
+                            console.log("Digite o Tamanho do arquivo (MB): ");
+                            tamanhoArquivo = readlinesync.questionFloat("");
+                            produtos.atualizar(
+                                new Ebook(id, tipo, titulo, autor, preco, tamanhoArquivo)
+                            );
+                            break;
+                    }
+
+                } else {
+                    console.log("\nO Livro de ID: " + id + " não foi encontrado!");
+                }
+
+                keyPress();
+                break;
+
+            case 5:
+                console.log("\n\nApagar Livro\n");
+
+                console.log("Digite o ID do Livro: ");
+                id = readlinesync.questionInt("");
+                produtos.deletar(id);
+
+                keyPress();
+                break;
+
+            default:
+                console.log("\nOpção inválida! Tente novamente.\n");
+
+                keyPress();
+                break;
+        }
     }
-
 }
+
+function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: Kefilwe Lourenço");
+    console.log("Generation Brasil");
+    console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log("\nPressione Enter para continuar...");
+    readlinesync.prompt();
 }
 
 main();
---
+++ src/controller/ProdutoController.ts
@@ -0,0 +1,67 @@
+import { Produto } from "../model/Produto";
+import { ProdutoRepository } from "../repository/ProdutoRepository";
+
+export class ProdutoController implements ProdutoRepository {
+
+    private listaProdutos: Array<Produto> = new Array<Produto>();
+    id: number = 0;
+
+    listarTodos(): void {
+        for(let produto of this.listaProdutos) {
+            produto.visualizar();
+        }
+    }
+
+    procurarPorId(id: number): void {
+        let buscaProduto = this.buscarNoArray(id);
+
+        if(buscaProduto != null) {
+            buscaProduto.visualizar();
+        }else
+            console.log("\nO produto de ID: " + id + "não foi encontrado!");
+    }
+
+    cadastrar(produto: Produto): void {
+        this.listaProdutos.push(produto);
+        console.log("\nO produto de ID: " + produto.id +
+            " foi cadastrado com sucesso!");
+    }
+
+    atualizar(produto: Produto): void {
+        let buscaProduto = this.buscarNoArray(produto.id);
+
+        if (buscaProduto != null) {
+            this.listaProdutos[this.listaProdutos.indexOf(buscaProduto)] = produto;
+            console.log("\nO produto de ID: " + produto.id +
+                " foi atualizado com sucesso!");
+        }else
+            console.log("\nO produto de ID: " + produto.id +
+                " não foi encontrado!");
+    }
+    
+    public deletar(id: number): void {
+        let buscaProduto = this.buscarNoArray(id);
+
+        if (buscaProduto != null) {
+            this.listaProdutos.splice(this.listaProdutos.indexOf(buscaProduto), 1);
+            console.log("\nO produto de ID: " + id +
+                " foi apagado com sucesso!");
+        }else
+            console.log("\nO produto de ID: " + id +
+                " não foi encontrado!");
+    }
+
+    public gerarId(): number {
+        return ++ this.id;
+    }
+
+    public buscarNoArray(id: number): Produto | null {
+
+        for (let produto of this.listaProdutos) {
+            if(produto.id === id)
+                return produto;
+        }
+
+        return null;
+    }
+}
